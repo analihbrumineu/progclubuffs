57444ec6b5b56ae465042f799665ccf506d00b8d
Analih Brum Ineu <analihineu@hotmail.com>
2026-04-02 16:25:18 -0300

    Add solution for Beecrowd problem 1142

---
+++ turma-lovelace/beecrowd/1142.py
@@ -0,0 +1,13 @@
+contador = 0
+
+n = int(input())
+
+for num in range(n):
+    contador += 1
+    print(contador, end=' ')
+    contador += 1
+    print(contador, end=' ')
+    contador += 1
+    print(contador, end=' ')
+    contador += 1
+    print('PUM')
